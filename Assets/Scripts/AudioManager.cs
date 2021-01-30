8b7eef441e9f58ee1bd831bc7985e8a39269620c
laikawing <feelinggame.kawing@gmail.com>
2021-01-31 03:59:21 +0800

    end scene audiomanager

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,18 +1,49 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private static AudioManager _instance;
+
+    public static AudioManager Instance
     {
-        
+        get
+        {
+            if (!_instance)
+            {
+                _instance = FindObjectOfType<AudioManager>();
+            }
+            return _instance;
+        }    
     }
 
-    // Update is called once per frame
-    void Update()
+    public List<AudioSource> clipList;
+
+    public int currentClipIndex = 0;
+    public float currentTime;
+
+    private void Start()
     {
-        
+        clipList[currentClipIndex].Play();
+        currentClipIndex++;
+    }
+
+    public void nextClip()
+    {
+        if (currentClipIndex < clipList.Capacity)
+        {
+            clipList[currentClipIndex].time = clipList[currentClipIndex-1].time;
+            clipList[currentClipIndex-1].Stop();
+            clipList[currentClipIndex].Play();
+            currentClipIndex++;
+        }
+    }
+
+    public void stopAllClip()
+    {
+        clipList[currentClipIndex-1].Stop();
     }
 }
