26cca9f45681f3576b58504038d8a69913c137a3
laikawing <feelinggame.kawing@gmail.com>
2021-01-31 06:17:14 +0800

    start end

---
+++ Assets/Scripts/ScenesManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScenesManager : MonoBehaviour
+{
+    private static ScenesManager _instance;
+
+    public static ScenesManager Instance
+    {
+        get
+        {
+            if (!_instance)
+            {
+                _instance = FindObjectOfType<ScenesManager>();
+            }
+            return _instance;
+        }    
+    }
+
+    public void GoToStartScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+    
+    public void GoToGameScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+    
+    public void GoToEndScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(2);
+    }
+}
